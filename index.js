d58e8f81834fc3d42416ff52188f644a9be46b8b
facundoarielveron10 <facundoarielveron10@gmail.com>
2023-03-08 23:55:06 -0300

    Agregando Cors al proyecto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ---- IMPORTACIONES ---- //
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectarDB from './config/database.js';
 import UsuarioRoutes from './routes/UsuarioRoutes.js';
 import ProyectoRoutes from './routes/ProyectoRoutes.js';
@@ -19,6 +20,21 @@ dotenv.config();
 
 // BASE DE DATOS
 conectarDB();
+
+// PERMITIR CONEXIONES DESDE EL DOMINO DEL FRONTEND
+const whitelist = [`${process.env.FRONT_URL}`];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.includes(origin)) {
+			// TIENE LOS PERMISOS PARA CONSULTAR LA API
+			callback(null, true);
+		} else {
+			// NO TIENE LOS PERMISOS PARA CONSULTAR LA API
+			callback(new Error('Error de Cors'));
+		}
+	},
+};
+app.use(cors(corsOptions));
 
 // ROUTING
 app.use('/api/usuarios', UsuarioRoutes);
