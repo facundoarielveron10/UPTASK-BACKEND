c47dcfcca3d14e9319a277d907e518d11a4bd5b8
Facundo Veron <facu_ve_2016@hotmail.com>
2023-04-20 23:24:33 -0300

    Haciendo Cambios en la entrega de un Proyecto

--- controllers/ProyectoController.js
+++ controllers/ProyectoController.js
@@ -5,119 +5,113 @@ import Tarea from '../models/Tarea.js';
 
 // ---- CONTROLADOR (PROYECTOS) ---- //
 const obtenerProyectos = async (req, res) => {
-	// OBTENER TODOS LOS PROYECTOS ASOCIADOS AL USUARIO
-	const proyectos = await Proyecto.find()
-		.where('creador')
-		.equals(req.usuario);
+    // OBTENER TODOS LOS PROYECTOS ASOCIADOS AL USUARIO
+    const proyectos = await Proyecto.find()
+        .where('creador')
+        .equals(req.usuario);
 
-	// Retornamos los proyectos
-	res.json(proyectos);
+    // Retornamos los proyectos
+    res.json(proyectos);
 };
 
 const nuevoProyecto = async (req, res) => {
-	// CREACION DE UN NUEVO PROYECTO
-	const proyecto = new Proyecto(req.body);
-	proyecto.creador = req.usuario._id;
+    // CREACION DE UN NUEVO PROYECTO
+    const proyecto = new Proyecto(req.body);
+    proyecto.creador = req.usuario._id;
 
-	try {
-		// Guardamos en la base de datos
-		const proyectoAlmacenado = await proyecto.save();
-		// Retornamos el proyecto creado
-		res.json(proyectoAlmacenado);
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        // Guardamos en la base de datos
+        const proyectoAlmacenado = await proyecto.save();
+        // Retornamos el proyecto creado
+        res.json(proyectoAlmacenado);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const obtenerProyecto = async (req, res) => {
-	// OBTENER UN PROYECTO ELEGIDO POR EL USUARIO
-	const { id } = req.params;
+    // OBTENER UN PROYECTO ELEGIDO POR EL USUARIO
+    const { id } = req.params;
 
-	// VERIFICAR LA EXISTENCIA DEL PROYECTO
-	const proyecto = await Proyecto.findById(id);
+    // VERIFICAR LA EXISTENCIA DEL PROYECTO
+    const proyecto = await Proyecto.findById(id);
 
-	if (!proyecto) {
-		const error = new Error('Proyecto no encontrado');
-		return res.status(404).json({ msg: error.message });
-	}
+    if (!proyecto) {
+        const error = new Error('Proyecto no encontrado');
+        return res.status(404).json({ msg: error.message });
+    }
 
-	// VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
-	if (proyecto.creador.toString() !== req.usuario._id.toString()) {
-		const error = new Error('Accion no valida');
-		return res.status(401).json({ msg: error.message });
-	}
+    // VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Accion no valida');
+        return res.status(401).json({ msg: error.message });
+    }
 
-	// OBTENER LAS TAREAS DEL PROYECTO
-	const tareas = await Tarea.find().where('proyecto').equals(proyecto._id);
-
-	// RETORNAR LOS PROYECTOS Y LAS TAREAS
-	res.json({
-		proyecto,
-		tareas,
-	});
+    // RETORNAR LOS PROYECTOS Y LAS TAREAS
+    res.json(proyecto);
 };
 
 const editarProyecto = async (req, res) => {
-	// EDITAR PROYECTO SELECCIONADO POR EL USUARIO
-	const { id } = req.params;
+    // EDITAR PROYECTO SELECCIONADO POR EL USUARIO
+    const { id } = req.params;
 
-	// VERIFICAR LA EXISTENCIA DEL PROYECTO
-	const proyecto = await Proyecto.findById(id);
+    // VERIFICAR LA EXISTENCIA DEL PROYECTO
+    const proyecto = await Proyecto.findById(id);
 
-	if (!proyecto) {
-		const error = new Error('Proyecto no encontrado');
-		return res.status(404).json({ msg: error.message });
-	}
+    if (!proyecto) {
+        const error = new Error('Proyecto no encontrado');
+        return res.status(404).json({ msg: error.message });
+    }
 
-	// VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
-	if (proyecto.creador.toString() !== req.usuario._id.toString()) {
-		const error = new Error('Accion no valida');
-		return res.status(401).json({ msg: error.message });
-	}
+    // VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Accion no valida');
+        return res.status(401).json({ msg: error.message });
+    }
 
-	// EDITAR PROYECTO
-	proyecto.nombre = req.body.nombre || proyecto.nombre;
-	proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
-	proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
-	proyecto.cliente = req.body.cliente || proyecto.cliente;
+    // EDITAR PROYECTO
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+    proyecto.cliente = req.body.cliente || proyecto.cliente;
 
-	try {
-		// Guardamos en la base de datos el proyecto
-		const proyectoAlmacenado = await proyecto.save();
-		// Retornamos el proyecto
-		res.json(proyectoAlmacenado);
-	} catch (error) {
-		// Mostramos el error
-		console.log(error);
-	}
+    try {
+        // Guardamos en la base de datos el proyecto
+        const proyectoAlmacenado = await proyecto.save();
+        // Retornamos el proyecto
+        res.json(proyectoAlmacenado);
+    } catch (error) {
+        // Mostramos el error
+        console.log(error);
+    }
 };
 
 const eliminarProyecto = async (req, res) => {
-	// ELIMINAR PROYECTO SELECCIONADO POR EL USUARIO
-	const { id } = req.params;
+    // ELIMINAR PROYECTO SELECCIONADO POR EL USUARIO
+    const { id } = req.params;
 
-	// VERIFICAR LA EXISTENCIA DEL PROYECTO
-	const proyecto = await Proyecto.findById(id);
+    // VERIFICAR LA EXISTENCIA DEL PROYECTO
+    const proyecto = await Proyecto.findById(id);
 
-	if (!proyecto) {
-		const error = new Error('Proyecto no encontrado');
-		return res.status(404).json({ msg: error.message });
-	}
+    if (!proyecto) {
+        const error = new Error('Proyecto no encontrado');
+        return res.status(404).json({ msg: error.message });
+    }
 
-	// VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
-	if (proyecto.creador.toString() !== req.usuario._id.toString()) {
-		const error = new Error('Accion no valida');
-		return res.status(401).json({ msg: error.message });
-	}
+    // VERIFICAR QUE EL PROYECTO LE PERTENESCA AL USUARIO
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Accion no valida');
+        return res.status(401).json({ msg: error.message });
+    }
 
-	// ELIMINAR PROYECTO
-	try {
-		await proyecto.deleteOne();
-		res.json({ msg: 'Proyecto Eliminado' });
-	} catch (error) {
-		// Mostramos el error
-		console.log(error);
-	}
+    // ELIMINAR PROYECTO
+    try {
+        await proyecto.deleteOne();
+        res.json({ msg: 'Proyecto Eliminado' });
+    } catch (error) {
+        // Mostramos el error
+        console.log(error);
+    }
 };
 
 const agregarColaborador = async (req, res) => {};
@@ -127,12 +121,12 @@ const eliminarColaborador = async (req, res) => {};
 
 // ---- EXPORTACIONES ---- //
 export {
-	obtenerProyectos,
-	nuevoProyecto,
-	obtenerProyecto,
-	editarProyecto,
-	eliminarProyecto,
-	agregarColaborador,
-	eliminarColaborador,
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
 };
 // ---- ---- ---- ---- ---- //
